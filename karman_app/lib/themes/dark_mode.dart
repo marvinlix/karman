10097c69536c53ea67c6b208a0f5be1aa70c54d7
surtecha <suryatejachalla.dev@gmail.com>
2024-06-23 10:51:53 +0530

    feat: theme data

---
+++ karman_app/lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    primary: Colors.grey[900]!,
+    secondary: Colors.grey[800]!,
+    background: Colors.black,
+  ),
+);
