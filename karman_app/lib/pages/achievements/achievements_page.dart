82e22644083600d35d7e3db14a98da1da6a2aac3
surtecha <suryatejachalla.dev@gmail.com>
2024-08-09 12:19:35 +0530

    chore: fix sliver refresh for habits and achievements page
    fix: achievement names

--- karman_app/lib/pages/achievements/achievements_page.dart
+++ karman_app/lib/pages/achievements/achievements_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:karman_app/constants/achievements_constants.dart';
 import 'package:karman_app/services/achievement/achievement_service.dart';
+import 'dart:async';
 
 class AchievementsPage extends StatefulWidget {
   const AchievementsPage({super.key});
@@ -12,34 +13,55 @@ class AchievementsPage extends StatefulWidget {
 class _AchievementsPageState extends State<AchievementsPage> {
   final AchievementService _achievementService = AchievementService();
   Map<String, bool> unlockedAchievements = {};
-  bool _isLoading = true;
+  bool _isInitialLoad = true;
+  Timer? _debounce;
 
   @override
   void initState() {
     super.initState();
-    _loadAchievements();
+    _initialLoadAchievements();
   }
 
-  Future<void> _loadAchievements() async {
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _initialLoadAchievements() async {
+    final newUnlockedAchievements =
+        await _achievementService.checkAchievements();
     setState(() {
-      _isLoading = true;
+      unlockedAchievements = newUnlockedAchievements;
+      _isInitialLoad = false;
     });
-    unlockedAchievements = await _achievementService.checkAchievements();
-    setState(() {
-      _isLoading = false;
+  }
+
+  Future<void> _refreshAchievements() async {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 300), () async {
+      final newUnlockedAchievements =
+          await _achievementService.checkAchievements();
+      setState(() {
+        unlockedAchievements = newUnlockedAchievements;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Achievements'),
+        backgroundColor: CupertinoColors.black,
+      ),
       child: SafeArea(
-        child: _isLoading
+        child: _isInitialLoad
             ? Center(child: CupertinoActivityIndicator())
             : CustomScrollView(
                 slivers: [
                   CupertinoSliverRefreshControl(
-                    onRefresh: _loadAchievements,
+                    onRefresh: _refreshAchievements,
                   ),
                   SliverList(
                     delegate: SliverChildBuilderDelegate(
@@ -64,44 +86,50 @@ class _AchievementsPageState extends State<AchievementsPage> {
   }
 
   Widget _buildAchievementTile(Achievement achievement, bool isUnlocked) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: Row(
-        children: [
-          Icon(
-            isUnlocked ? CupertinoIcons.rosette : CupertinoIcons.lock,
-            size: 40,
-            color:
-                isUnlocked ? CupertinoColors.white : CupertinoColors.systemGrey,
-          ),
-          SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  achievement.name,
-                  style: TextStyle(
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 300),
+      child: Container(
+        key: ValueKey('${achievement.name}_$isUnlocked'),
+        padding: EdgeInsets.all(16),
+        child: Row(
+          children: [
+            Icon(
+              isUnlocked ? CupertinoIcons.rosette : CupertinoIcons.lock,
+              size: 40,
+              color: isUnlocked
+                  ? CupertinoColors.white
+                  : CupertinoColors.systemGrey,
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    achievement.name,
+                    style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       color: isUnlocked
                           ? CupertinoColors.white
-                          : CupertinoColors.systemGrey),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  achievement.description,
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: isUnlocked
-                        ? CupertinoColors.white
-                        : CupertinoColors.systemGrey,
+                          : CupertinoColors.systemGrey,
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 4),
+                  Text(
+                    achievement.description,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: isUnlocked
+                          ? CupertinoColors.white
+                          : CupertinoColors.systemGrey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- karman_app/lib/pages/habit/habits_page.dart
+++ karman_app/lib/pages/habit/habits_page.dart
@@ -4,6 +4,7 @@ import 'package:karman_app/controllers/habit/habit_controller.dart';
 import 'package:karman_app/models/habits/habit.dart';
 import 'package:karman_app/pages/habit/habit_details_sheet.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 
 class HabitsPage extends StatefulWidget {
   const HabitsPage({super.key});
@@ -13,14 +14,36 @@ class HabitsPage extends StatefulWidget {
 }
 
 class _HabitsPageState extends State<HabitsPage> {
+  bool _isLoading = true;
+  Timer? _debounce;
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    _loadHabits();
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _loadHabits() async {
+    final habitController =
+        Provider.of<HabitController>(context, listen: false);
+    await habitController.loadHabits();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _refreshHabits() async {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () async {
       final habitController =
           Provider.of<HabitController>(context, listen: false);
-      habitController.loadHabits();
-      habitController.scheduleReminders();
+      await habitController.loadHabits();
     });
   }
 
@@ -51,7 +74,9 @@ class _HabitsPageState extends State<HabitsPage> {
             middle: Padding(
               padding: const EdgeInsets.only(top: 20.0),
               child: Text(
-                '$incompleteHabits habits left',
+                habitController.habits.isEmpty
+                    ? 'Habits'
+                    : '$incompleteHabits habits left',
                 style: TextStyle(
                   color: CupertinoColors.white,
                   fontWeight: FontWeight.bold,
@@ -68,29 +93,50 @@ class _HabitsPageState extends State<HabitsPage> {
             ),
           ),
           child: SafeArea(
-            child: habitController.habits.isEmpty
-                ? Center(
-                    child: Text(
-                      'No habits yet. Add one to get started!',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: CupertinoColors.systemGrey,
+            child: _isLoading
+                ? Center(child: CupertinoActivityIndicator())
+                : habitController.habits.isEmpty
+                    ? _buildEmptyState()
+                    : CustomScrollView(
+                        slivers: [
+                          CupertinoSliverRefreshControl(
+                            onRefresh: _refreshHabits,
+                          ),
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                if (index < habitController.habits.length) {
+                                  final habit = habitController.habits[index];
+                                  return AnimatedSwitcher(
+                                    duration: Duration(milliseconds: 300),
+                                    child: HabitTile(
+                                      key: ValueKey(habit.habitId),
+                                      habit: habit,
+                                    ),
+                                  );
+                                }
+                                return SizedBox(height: 40);
+                              },
+                              childCount: habitController.habits.length + 1,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  )
-                : Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: ListView.builder(
-                      itemCount: habitController.habits.length,
-                      itemBuilder: (context, index) {
-                        final habit = habitController.habits[index];
-                        return HabitTile(habit: habit);
-                      },
-                    ),
-                  ),
           ),
         );
       },
+    );
+  }
+
+  Widget _buildEmptyState() {
+    return Center(
+      child: Text(
+        'No habits yet. Add one to get started!',
+        style: TextStyle(
+          fontSize: 18,
+          color: CupertinoColors.systemGrey,
+        ),
+      ),
     );
   }
 }
