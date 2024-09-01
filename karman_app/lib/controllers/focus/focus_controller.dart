8e32664449365f39153d9a34e11c4f29cdab9b72
surtecha <suryatejachalla.dev@gmail.com>
2024-09-01 18:49:09 +0530

    chore: Refactor code structure for focus feature

---
+++ karman_app/lib/controllers/focus/focus_controller.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:karman_app/manager/sound_manager.dart';
+import 'package:karman_app/services/timer_service.dart';
+import 'package:karman_app/services/focus_service.dart';
+import 'package:karman_app/services/achievement_service.dart';
+
+class FocusController extends ChangeNotifier {
+  final SoundManager _soundManager = SoundManager();
+  final TimerService _timerService = TimerService();
+  final FocusService _focusService = FocusService();
+  final AchievementService _achievementService = AchievementService();
+
+  int _timerValue = 5;
+  bool _isTimerRunning = false;
+  late DateTime _endTime;
+  Timer? _timer;
+  int _remainingSeconds = 300;
+  int _totalSeconds = 300;
+
+  int get timerValue => _timerValue;
+  bool get isTimerRunning => _isTimerRunning;
+  int get remainingSeconds => _remainingSeconds;
+  int get totalSeconds => _totalSeconds;
+  SoundManager get soundManager => _soundManager;
+
+  FocusController() {
+    _loadTimerState();
+  }
+
+  Future<void> _loadTimerState() async {
+    final timerState = await _timerService.getTimerState();
+    _timerValue = timerState['timerValue'];
+    _isTimerRunning = timerState['isTimerRunning'];
+    _remainingSeconds = timerState['remainingSeconds'];
+    _totalSeconds = timerState['totalSeconds'];
+    notifyListeners();
+    if (_isTimerRunning) {
+      _startTimer();
+      _soundManager.playSelectedSound();
+    }
+  }
+
+  void onSliderValueChanged(int value) {
+    _timerValue = value;
+    _remainingSeconds = value * 60;
+    _totalSeconds = _remainingSeconds;
+    _saveTimerState();
+    notifyListeners();
+  }
+
+  void toggleTimer() {
+    _isTimerRunning = !_isTimerRunning;
+    if (_isTimerRunning) {
+      _startTimer();
+      _soundManager.playSelectedSound();
+    } else {
+      _stopTimer(playChime: false);
+    }
+    _saveTimerState();
+    notifyListeners();
+  }
+
+  void _startTimer() {
+    _endTime = DateTime.now().add(Duration(seconds: _remainingSeconds));
+    _updateTimer();
+    _timer = Timer.periodic(Duration(seconds: 1), (_) => _updateTimer());
+  }
+
+  void _updateTimer() {
+    final now = DateTime.now();
+    if (now.isBefore(_endTime)) {
+      _remainingSeconds = _endTime.difference(now).inSeconds;
+      _saveTimerState();
+      notifyListeners();
+    } else {
+      _stopTimer(playChime: true);
+    }
+  }
+
+  void _stopTimer({required bool playChime}) {
+    _timer?.cancel();
+    _isTimerRunning = false;
+    _soundManager.stopBackgroundSound();
+
+    if (playChime) {
+      Future.delayed(Duration(seconds: 1), () {
+        _soundManager.playChime();
+      });
+      _recordFocusSession();
+    } else {
+      _remainingSeconds = _timerValue * 60;
+      _totalSeconds = _remainingSeconds;
+    }
+    _saveTimerState();
+    notifyListeners();
+  }
+
+  Future<void> _saveTimerState() async {
+    await _timerService.saveTimerState(
+      timerValue: _timerValue,
+      isTimerRunning: _isTimerRunning,
+      remainingSeconds: _remainingSeconds,
+      totalSeconds: _totalSeconds,
+    );
+  }
+
+  void _recordFocusSession() async {
+    final duration = _timerValue * 60;
+    await _focusService.addFocusSession(duration);
+
+    Map<String, bool> unlockedAchievements =
+        await _achievementService.checkAchievements();
+
+    _remainingSeconds = _timerValue * 60;
+    _totalSeconds = _remainingSeconds;
+    _saveTimerState();
+    notifyListeners();
+
+    if (unlockedAchievements.values.any((value) => value)) {
+      // TODO: Notify the UI to show achievements
+      // Use a callback or a stream for this
+    }
+  }
+
+  String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int remainingSeconds = seconds % 60;
+    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+  }
+
+  double get progress {
+    if (_totalSeconds == 0) return 0;
+    return (_totalSeconds - _remainingSeconds) / _totalSeconds * 115 + 5;
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _soundManager.dispose();
+    super.dispose();
+  }
+}
--- karman_app/lib/pages/focus/focus_page.dart
+++ karman_app/lib/pages/focus/focus_page.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
+import 'package:karman_app/controllers/focus/focus_controller.dart';
+import 'package:karman_app/pages/tutorial/focus_tutorial.dart';
+import 'package:provider/provider.dart';
 import 'package:karman_app/components/focus/circular_slider.dart';
 import 'package:karman_app/components/focus/rolling_menu.dart';
-import 'package:karman_app/manager/sound_manager.dart';
-import 'package:karman_app/database/database_service.dart';
-import 'package:karman_app/database/focus_db.dart';
-import 'package:karman_app/services/achievement_service.dart';
-import 'package:karman_app/services/timer_service.dart';
-import 'package:karman_app/pages/tutorial/focus_tutorial.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FocusPage extends StatefulWidget {
@@ -18,16 +14,6 @@ class FocusPage extends StatefulWidget {
 }
 
 class _FocusPageState extends State<FocusPage> with TickerProviderStateMixin {
-  int _timerValue = 5;
-  bool _isTimerRunning = false;
-  late Timer _timer;
-  int _remainingSeconds = 300;
-  int _totalSeconds = 300;
-  final SoundManager _soundManager = SoundManager();
-  final FocusDatabase _focusDatabase = FocusDatabase();
-  final AchievementService _achievementService = AchievementService();
-  final TimerService _timerService = TimerService();
-
   late AnimationController _animationController;
   late Animation<double> _animation;
   bool _isMenuOpen = false;
@@ -39,6 +25,13 @@ class _FocusPageState extends State<FocusPage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
+    _setupAnimations();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkFirstLaunch();
+    });
+  }
+
+  void _setupAnimations() {
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 300),
       vsync: this,
@@ -50,24 +43,6 @@ class _FocusPageState extends State<FocusPage> with TickerProviderStateMixin {
     );
     _tutorialFadeAnimation = Tween<double>(begin: 0.0, end: 1.0)
         .animate(_tutorialAnimationController);
-    _loadTimerState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _checkFirstLaunch();
-    });
-  }
-
-  Future<void> _loadTimerState() async {
-    final timerState = await _timerService.getTimerState();
-    setState(() {
-      _timerValue = timerState['timerValue'];
-      _isTimerRunning = timerState['isTimerRunning'];
-      _remainingSeconds = timerState['remainingSeconds'];
-      _totalSeconds = timerState['totalSeconds'];
-    });
-    if (_isTimerRunning) {
-      _startTimer();
-      _soundManager.playSelectedSound();
-    }
   }
 
   Future<void> _checkFirstLaunch() async {
@@ -91,132 +66,6 @@ class _FocusPageState extends State<FocusPage> with TickerProviderStateMixin {
     await prefs.setBool('first_launch_focus', false);
   }
 
-  @override
-  void dispose() {
-    _timer.cancel();
-    _soundManager.dispose();
-    _animationController.dispose();
-    _tutorialAnimationController.dispose();
-    super.dispose();
-  }
-
-  void _onSliderValueChanged(int value) {
-    setState(() {
-      _timerValue = value;
-      _remainingSeconds = value * 60;
-      _totalSeconds = _remainingSeconds;
-    });
-    _saveTimerState();
-  }
-
-  void _toggleTimer() {
-    setState(() {
-      _isTimerRunning = !_isTimerRunning;
-      if (_isTimerRunning) {
-        _startTimer();
-        _soundManager.playSelectedSound();
-      } else {
-        _stopTimer(playChime: false);
-      }
-    });
-    _saveTimerState();
-  }
-
-  void _startTimer() {
-    _totalSeconds = _remainingSeconds;
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_remainingSeconds > 0) {
-          _remainingSeconds--;
-          _saveTimerState();
-        } else {
-          _stopTimer(playChime: true);
-        }
-      });
-    });
-  }
-
-  void _stopTimer({required bool playChime}) {
-    _timer.cancel();
-    _isTimerRunning = false;
-    _soundManager.stopBackgroundSound();
-
-    if (_isMenuOpen) {
-      _toggleMenu();
-    }
-
-    if (playChime) {
-      Future.delayed(Duration(seconds: 1), () {
-        _soundManager.playChime();
-      });
-
-      _recordFocusSession();
-    } else {
-      setState(() {
-        _remainingSeconds = _timerValue * 60;
-        _totalSeconds = _remainingSeconds;
-      });
-    }
-    _saveTimerState();
-  }
-
-  Future<void> _saveTimerState() async {
-    await _timerService.saveTimerState(
-      timerValue: _timerValue,
-      isTimerRunning: _isTimerRunning,
-      remainingSeconds: _remainingSeconds,
-      totalSeconds: _totalSeconds,
-    );
-  }
-
-  void _recordFocusSession() async {
-    final database = await DatabaseService().database;
-    final duration = _timerValue * 60;
-    final date = DateTime.now().toIso8601String().split('T')[0];
-    await _focusDatabase.addFocusSession(database, duration, date);
-
-    Map<String, bool> unlockedAchievements =
-        await _achievementService.checkAchievements();
-    _showNewAchievements(unlockedAchievements);
-
-    setState(() {
-      _remainingSeconds = _timerValue * 60;
-      _totalSeconds = _remainingSeconds;
-    });
-    _saveTimerState();
-  }
-
-  void _showNewAchievements(Map<String, bool> unlockedAchievements) {
-    List<String> newAchievements = unlockedAchievements.entries
-        .where((entry) => entry.value)
-        .map((entry) => entry.key)
-        .toList();
-
-    if (newAchievements.isNotEmpty) {
-      showCupertinoDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return CupertinoAlertDialog(
-            title: Text('New Achievements Unlocked!'),
-            content: Column(
-              children: newAchievements
-                  .map((achievement) => Text(achievement))
-                  .toList(),
-            ),
-            actions: [
-              CupertinoDialogAction(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
   void _toggleMenu() {
     setState(() {
       _isMenuOpen = !_isMenuOpen;
@@ -228,91 +77,95 @@ class _FocusPageState extends State<FocusPage> with TickerProviderStateMixin {
     });
   }
 
-  String _formatTime(int seconds) {
-    int minutes = seconds ~/ 60;
-    int remainingSeconds = seconds % 60;
-    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
-  }
-
-  double get _progress {
-    if (_totalSeconds == 0) return 0;
-    return (_totalSeconds - _remainingSeconds) / _totalSeconds * 115 + 5;
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _tutorialAnimationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: CupertinoColors.black,
-        trailing: _isTimerRunning
-            ? CupertinoButton(
-                onPressed: _isTimerRunning ? _toggleMenu : null,
-                child: Icon(
-                  _soundManager.currentIcon,
-                  color: _isTimerRunning
-                      ? CupertinoColors.white
-                      : CupertinoColors.systemGrey,
-                  size: 32,
-                ),
-              )
-            : null,
-      ),
-      child: SafeArea(
-        child: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Center(
-                child: CircularSlider(
-                  onValueChanged: _onSliderValueChanged,
-                  currentValue: _timerValue,
-                  isTimerRunning: _isTimerRunning,
-                  timeDisplay: _formatTime(_remainingSeconds),
-                  progress: _progress,
-                  onPlayPause: _toggleTimer,
-                ),
-              ),
+    return ChangeNotifierProvider(
+      create: (context) => FocusController(),
+      child: Consumer<FocusController>(
+        builder: (context, controller, child) {
+          return CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              backgroundColor: CupertinoColors.black,
+              trailing: controller.isTimerRunning
+                  ? CupertinoButton(
+                      onPressed: controller.isTimerRunning ? _toggleMenu : null,
+                      child: Icon(
+                        controller.soundManager.currentIcon,
+                        color: controller.isTimerRunning
+                            ? CupertinoColors.white
+                            : CupertinoColors.systemGrey,
+                        size: 32,
+                      ),
+                    )
+                  : null,
             ),
-            Positioned(
-              top: 0,
-              right: 0,
-              left: 0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
+            child: SafeArea(
+              child: Stack(
                 children: [
-                  SizedBox(height: 44),
-                  SizeTransition(
-                    sizeFactor: _animation,
-                    axisAlignment: -1,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 8),
-                      child: RollingMenu(
-                        items: _soundManager.sounds,
-                        onItemSelected: (Map<String, dynamic> sound) {
-                          setState(() {
-                            _soundManager.currentSound = sound['file'];
-                            if (sound['file'] == null) {
-                              _soundManager.stopBackgroundSound();
-                            } else if (_isTimerRunning) {
-                              _soundManager.playSelectedSound();
-                            }
-                            _toggleMenu();
-                          });
-                        },
-                        currentSound: _soundManager.currentSound,
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: CircularSlider(
+                        onValueChanged: controller.onSliderValueChanged,
+                        currentValue: controller.timerValue,
+                        isTimerRunning: controller.isTimerRunning,
+                        timeDisplay:
+                            controller.formatTime(controller.remainingSeconds),
+                        progress: controller.progress,
+                        onPlayPause: controller.toggleTimer,
                       ),
                     ),
                   ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(height: 44),
+                        SizeTransition(
+                          sizeFactor: _animation,
+                          axisAlignment: -1,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 8),
+                            child: RollingMenu(
+                              items: controller.soundManager.sounds,
+                              onItemSelected: (Map<String, dynamic> sound) {
+                                controller.soundManager.currentSound =
+                                    sound['file'];
+                                if (sound['file'] == null) {
+                                  controller.soundManager.stopBackgroundSound();
+                                } else if (controller.isTimerRunning) {
+                                  controller.soundManager.playSelectedSound();
+                                }
+                                _toggleMenu();
+                              },
+                              currentSound:
+                                  controller.soundManager.currentSound,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  if (_showTutorial)
+                    FadeTransition(
+                      opacity: _tutorialFadeAnimation,
+                      child: FocusTutorial.build(context, _onTutorialComplete),
+                    ),
                 ],
               ),
             ),
-            if (_showTutorial)
-              FadeTransition(
-                opacity: _tutorialFadeAnimation,
-                child: FocusTutorial.build(context, _onTutorialComplete),
-              ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
