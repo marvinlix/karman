f13ded63a41352b8be849b50e70f9e0350f76dc7
surtecha <suryatejachalla.dev@gmail.com>
2024-09-03 18:24:54 +0530

    refactor: Refactor community, contribution, and support pages to use lowercase "karman" instead of "Karman"

--- karman_app/lib/pages/more/details/support_page.dart
+++ karman_app/lib/pages/more/details/support_page.dart
@@ -6,9 +6,9 @@ final supportPage = SkeletonPage(
   linkText: 'Support the Project',
   linkUrl: 'https://github.com/sponsors/surtecha',
   paragraphs: const [
-    'Karman is a passion project, designed to be free and accessible to everyone, without ads or hidden fees.',
-    'To keep this vision alive, we rely on the generosity of users like you. Your donations help cover essential costs for maintenance and updates, ensuring that Karman remains available for all.',
-    'By supporting Karman, you become a part of something bigger—empowering a community-driven app that prioritizes its users\' needs.',
+    'karman is a passion project, designed to be free and accessible to everyone, without ads or hidden fees.',
+    'To keep this vision alive, we rely on the generosity of users like you. Your donations help cover essential costs for maintenance and updates, ensuring that karman remains available for all.',
+    'By supporting karman, you become a part of something bigger—empowering a community-driven app that prioritizes its users\' needs.',
     'Make a difference today with your contribution, no matter how small.'
   ],
 );
